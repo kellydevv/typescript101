103251ef063e7f1f504b1e28791ad4b345c53ab1
정현진 <jhj9328@hanmail.net>
2022-11-08 17:27:17 +0900

    event에 type 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import styled from "styled-components";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+const {currentTarget:{value}
+} =event;
+setValue(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    //event.preventDefault();
+    console.log("hello", value);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="username" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
         <button>login</button>
       </form>
     </div>
