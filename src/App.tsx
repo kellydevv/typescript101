b32c9155983132c6f05c5e321d7fbd67bcf7a8d9
정현진 <jhj9328@hanmail.net>
2022-11-08 16:48:00 +0900

    3.5수업을 위해 circle.tsx삭제 &App.tsx 업뎃

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import styled from "styled-components";
-import Circle from "./Circle";
-
 
 function App() {
   return (
     <div>
-        <Circle borderColor="yellow" bgColor="teal"/>
-        <Circle bgColor="tomato"/>
-      {/* component를 type을 한다== component에게 type을 준다==
-      typescript에게 어떤 type인지 설명한다 */}
+      <form>
+        <input type="text" placeholder="username" />
+        <button>login</button>
+      </form>
     </div>
-  )
+  );
 }
 
 export default App;
-
-// typescript에게 내 component가 가져야하는 prop을 설명한다. 
-// JS에서 prop types는 prop이 거기에 있는지 없는지 확인해주지만, 코드를 실행한
-// "후"에만 확인 가능하다.(브라우저콘솔)
-// TS를 사용하는 이유는 코드가 실행되기 "전"에 오류를 확인하기 위해서이다.
-// 그러므로TS에서는 prop types를 사용하지 않는다.
