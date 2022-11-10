e3ae517f75c860e0e3b9c2cf03c214d5ae8a5ecd
정현진 <jhj9328@hanmail.net>
2022-11-10 16:49:29 +0900

    infoData&priceData fetch

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,6 @@
-import { useParams, useLocation} from "react-router";
+import { useParams, useLocation } from "react-router";
 import styled from "styled-components";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Container = styled.div`
   padding: 0px 20px;
@@ -40,13 +40,30 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams() as unknown as Params;
   const { state } = useLocation() as LocationState;
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState();
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      console.log(infoData);
+      //1st await for json, 2nd await for fetch()
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      console.log(priceData);
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : null}
+      {loading ? <Loader>Loading...</Loader> : <span>{info.hello}</span>}
     </Container>
   );
 }
